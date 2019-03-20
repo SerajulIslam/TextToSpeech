51b8d3ba406306039c91fada36930a4caf1e4611
SerajulIslam <serajulislam666@gmail.com>
2019-03-20 09:00:46 +0600

    commit

---
+++ app/src/main/java/com/example/serajul/texttospeech/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.serajul.texttospeech;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech toSpeech;
+    int result;
+    TextView textView;
+    String text;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        toSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status== TextToSpeech.SUCCESS)
+                {
+                    result=toSpeech.setLanguage(Locale.US);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Feature not supported in your device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void TTS(View view){
+        switch (view.getId()){
+            case R.id.bspeak:
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                    Toast.makeText(this, "Feature not supported in your device", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    text = textView.getText().toString();
+                    toSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                }
+                break;
+            case R.id.bstop:
+                if (toSpeech!=null){
+                    toSpeech.stop();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (toSpeech!=null){
+            toSpeech.stop();
+            toSpeech.shutdown();
+        }
+    }
+}
+
